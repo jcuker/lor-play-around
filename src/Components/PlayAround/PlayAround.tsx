48086b4afe0c6d8d0134ecf39ef34c121dfecdca
Jordan Cuker <eliteforcejac@gmail.com>
2021-03-13 19:22:42 -0600

    mobile changes to around page

--- src/Components/PlayAround/PlayAround.tsx
+++ src/Components/PlayAround/PlayAround.tsx
@@ -46,7 +46,7 @@ export default function PlayAround() {
 
    return (
       <div className="flex flex-col">
-         <div className="flex flex-row justify-center flex-wrap">
+         <div className="flex flex-row justify-center flex-wrap gap-3 mb-4">
             {regions.map((region) => (
                <Region key={region} name={region} />
             ))}
