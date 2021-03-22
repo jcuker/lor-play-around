d139d0eea0220d61da9c92ebae5b458f74ee4217
Jordan Cuker <eliteforcejac@gmail.com>
2021-03-22 12:24:58 -0500

    remove old url  links

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,14 @@
 import Footer from "Components/Footer/Footer";
 import Landing from "Components/Landing/Landing";
-import PlayAround from "Components/PlayAround/PlayAround";
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.render(
    <React.StrictMode>
       <Router basename={process.env.PUBLIC_URL}>
-         <Switch>
-            <Route path="/around">
-               <PlayAround />
-            </Route>
-            <Route path="/">
-               <Landing />
-            </Route>
-         </Switch>
+         <Landing />
       </Router>
       <Footer />
    </React.StrictMode>,
